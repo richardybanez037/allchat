d682255f641f1b1a11f87d5682edc3959bc237dd
richardybanez037 <richardybanez037@gmail.com>
2025-02-25 23:13:36 +0800

    update - added message date time

--- app/components/message.tsx
+++ app/components/message.tsx
@@ -4,7 +4,11 @@ import { IMessage } from '../definitions';
 
 export default function Message({message}: {message: IMessage}){
   return <div className="message break-words border-2 border-gray-300 rounded p-2 text-gray-300 mb-2">
-    <h2 className="font-semibold">{message.messages_name}</h2>
+    <div className='flex gap-1'>
+      <h2 className="font-semibold">{message.messages_name}</h2>
+      <h2>&nbsp;&#8226;&nbsp;</h2>
+      <h2>{new Date(message.messages_created_at).toLocaleString()}</h2>
+    </div>
     <h2>{message.messages_content}</h2>
   </div>
 }
