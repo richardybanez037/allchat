bc039ba527252ea4170d7b448b7c799e640e1714
richardybanez037 <richardybanez037@gmail.com>
2025-02-26 18:25:59 +0800

    fix test - mobile - messages container

--- app/page.tsx
+++ app/page.tsx
@@ -52,10 +52,10 @@ export default function Home() {
   return (
     <div className='flex justify-center w-full'>
       <form 
-        className='flex flex-col gap-1 p-2 h-screen w-full md:w-[50em]'
+        className='flex flex-col gap-1 p-2 pb-0 pt-0 h-screen w-full md:w-[50em]'
         onSubmit={submitMessage}
       >
-        <div className='flex gap-1 items-center pb-2 sticky top-0'>
+        <div className='flex gap-1 items-center pb-2 sticky top-0 pt-2'>
           <label className="text-gray-300">Name:</label>
           <input 
             onChange={e => setNameInput(e.target.value)}
@@ -66,7 +66,7 @@ export default function Home() {
             required
             />
         </div>
-        <div className='grow border-2 border-gray-300 rounded p-2 pb-0 mb-2 overflow-y-scroll sticky top-12'>
+        <div className='grow border-2 border-gray-300 rounded p-2 pb-0 mb-2 overflow-y-scroll sticky'>
           {
             loading ? 
               <Loading/> :
@@ -74,16 +74,14 @@ export default function Home() {
           }
           <span ref={lastItemRef}></span>
         </div>
-        <div className="flex gap-2">
-          <div className="grow">
-            <input
-              onChange={e => setMessageInput(e.target.value)} 
-              value={messageInput}
-              name="message" 
-              className="rounded border w-full h-full p-2 outline-none bg-gray-100"
-              required
-            />
-          </div>
+        <div className="flex gap-2 pb-2">
+          <input
+            onChange={e => setMessageInput(e.target.value)} 
+            value={messageInput}
+            name="message" 
+            className="rounded border w-full h-full p-2 outline-none bg-gray-100"
+            required
+          />
           <div className='relative flex items-center justify-center'>
             <div className='absolute bottom-12 -right-16'>
               <EmojiPicker 
@@ -91,7 +89,6 @@ export default function Home() {
                 lazyLoadEmojis={true}
                 theme={Theme.DARK}
                 emojiStyle={EmojiStyle.NATIVE}
-                autoFocusSearch={true}
                 width={"50vw"}
                 onEmojiClick={(selected) => setMessageInput(prev => prev + selected.emoji)}
               />
